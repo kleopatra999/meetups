e77e037c8804899968e4cb8a49d15ad8e0830cb2
Clemens Helm <clemens.helm@gmail.com>
2013-12-18 12:15:33 +0100

    brings multicity config and sorting

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var FILTERS = [
+  {
+    country: 'AT',
+    category_id: 34,
+    city: 'Vienna'
+  },
+  {
+    country: 'AT',
+    category_id: 34,
+    city: 'Salzburg'
+  },
+  {
+    country: 'CZ',
+    category_id: 34,
+    city: 'Prague'
+  },
+  {
+    country: 'CZ',
+    category_id: 34,
+    city: 'Brno'
+  },
+  {
+    country: 'HU',
+    category_id: 34,
+    city: 'Budapest'
+  },
+  {
+    country: 'AT',
+    category_id: 34,
+    city: 'Graz'
+  },
+  {
+    country: 'PL',
+    category_id: 34,
+    city: 'Krakow'
+  },
+];
+
 function parseEntry(entry) {
   return {
     'id':         entry.id,
@@ -12,6 +50,7 @@ function parseEntry(entry) {
 }
 
 function parseResult(result) {
+  if (!result.results) return [];
   var ret = [];
   for (var i =0; i < result.results.length; i++) {
     ret.push(parseEntry(result.results[i]));
@@ -19,20 +58,34 @@ function parseResult(result) {
   return ret;
 }
 
+function generateUrl(filter) {
+  return 'https://api.meetup.com/2/open_events?' +
+            'category_id=' + filter.category_id +
+            '&country=' + filter.country +
+            '&city=' + filter.city +
+            '&radius=100' +
+            '&key=7013cc73581ac377c5372713c133';
+} 
+
 function TodoCtrl($scope, $http) {
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
-  
-    $http({
-      method: 'JSONP',
-      url: "https://api.meetup.com/2/open_events?category_id=34&country=AT&city=Vienna&radius=300&key=7013cc73581ac377c5372713c133&callback=JSON_CALLBACK"
-    }).
-    success(function(data, status, headers, config) {
-      console.log(data);
-      $scope.meetups = parseResult(data);
-      console.log($scope.meetups);
-    }).
-    error(function(data, status, headers, config) {
-    });
+  $scope.meetups = [];
+  $scope.meetupIds = [];
+
+    for (var i = 0; i < FILTERS.length; i++) {
+      $http({
+        method: 'JSONP',
+        url: generateUrl(FILTERS[i]) + '&callback=JSON_CALLBACK'
+      }).
+      success(function(data, status, headers, config) {
+        var res = parseResult(data);
+        for (var j = 0; j < res.length; j++) {
+          if ($scope.meetupIds.indexOf(res[j].id) < 0) {
+            $scope.meetups.push(res[j]);
+            $scope.meetupIds.push(res[j].id);
+          }
+        }
+      }).
+      error(function(data, status, headers, config) {
+      });
+    }
 }
